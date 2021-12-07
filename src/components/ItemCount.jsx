5581f3a197bb565ed8c97b0acb1bbdc9f5b39e04
Rocio Godoy <rgodoylarrosa@gmail.com>
2021-12-07 01:47:33 -0300

    Add stock control to ItemCount component

--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -1,15 +1,22 @@
+import React from 'react'
 import { useState} from "react";
 import { Button } from "react-bootstrap";
 
+
 function ItemCount ({ stock, initial, onAdd }){
     const [quantity, setQuantity] = useState(initial);
-    function onAdd ({ value }) {
-        setQuantity(quantity+value);
+    function onAdd (value) {
+        if (value>0 && quantity+1<=stock){
+            setQuantity(parseInt(quantity) + parseInt(value));
+        } else if (value<0 && quantity>=1){
+            setQuantity(parseInt(quantity) + parseInt(value));
+        }
     }
+
     return (
     <div>
         <Button variant="dark" onClick={()=>onAdd(1)}> + </Button>    
-        <input size="5" type="text" value ={quantity} />
+        <input size="5" value ={quantity} />
         <Button variant="dark" onClick={()=>onAdd(-1)}> - </Button>
     </div>)
 }
