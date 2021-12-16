4689fafc7256178e22cd4f5f11d19ff010a04a9e
Rocio Godoy <rgodoylarrosa@gmail.com>
2021-12-16 02:27:10 -0300

    Add navigation from Card to Item

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,17 +1,23 @@
 import React from 'react'
 import { Card, Button } from "react-bootstrap";
+import { Link } from 'react-router-dom';
 //id, title, description, price, pictureUrl 
 const Item = ({key,item}) => {
-return (<Card className="text-center" style={{ width: '18rem' }}>
-            <Card.Img variant="top" src={item.pictureUrl} className='card-img' />
-            <Card.Body>
-                <Card.Title>{item.title}</Card.Title>
-                <Card.Text>
-                {item.description}.
-                </Card.Text>
-                <Button variant="secondary" size="lg">
-                Add cart
-                </Button>
-            </Card.Body>
-        </Card>)};
+return (
+        <Link to={`/item/${item.id}`}>
+            <Card className="text-center" style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={item.pictureUrl} className='card-img' />
+                <Card.Body>
+                    <Card.Title>{item.title}</Card.Title>
+                    <Card.Text>
+                    {item.description}.
+                    </Card.Text>
+                    <Button variant="secondary" size="lg">
+                    Add cart
+                    </Button>
+                </Card.Body>
+            </Card>
+        </Link>    
+        )};
+        
 export default Item;
