dfca2083cc4b31c8ce0c4a00109686442716fa48
Rocio Godoy <rgodoylarrosa@gmail.com>
2021-12-16 01:43:40 -0300

    Add Link in NavBar

--- src/components/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer.jsx
@@ -6,7 +6,7 @@ import { useParams } from 'react-router-dom';
 
 export default function ItemDetailContainer() {
     const [item, setItem] = useState();
-    const { itemId } = useParams (); 
+    const { id } = useParams (); 
     let itemData= { 'id':'1', 'title':'PHILODENDRON NARROW “JUNGLE BOOGIE” IN 4-INCH POT', 'description':'“Philodendron Jungle Boogie Plants are an interesting plant for interior decorating. Tiger Tooth Philodendron has glossy green, deeply cut leaves that create a beautiful tropical accent in any room or office. This philodendron has very few problems and tolerates neglect and dry indoor air.”', 'price':15, 'pictureUrl':'https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1610733391-il_794xN.2480917409_qe1e.jpg' }
     
     let promise = new Promise ((resolve) => {
@@ -25,12 +25,12 @@ export default function ItemDetailContainer() {
     }
 
     useEffect(() => {
-        getItem()}, [itemId]);
+        getItem()}, [id]);
     
     return (
         <div id="main">
             <div className="itempcontainer">
-            <p>Recibo itemId: {itemId}</p>
+            <p>Recibo itemId: {id}</p>
             {item && <ItemDetail item={item}/>    } 
             </div>
         </div>
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route} from 'react-dom-routes'
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
 import ItemDetailContainer from '../components/ItemDetailContainer'
 import ItemListContainer from '../components/ItemListContainer'
+import NavBar from '../components/NavBar'
 
 const AppRoutes = () => {
     return (
         <BrowserRouter>
+            <NavBar/>
             <Routes>
                 <Route path='/' element={<ItemListContainer/>}/>
                 <Route path='/category/:id' element={<ItemListContainer/>}/>
